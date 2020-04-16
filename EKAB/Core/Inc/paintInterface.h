60b80f759e1cdc90cc75dbaedb9fdb76a0e8f557
EmKalin <61980221+EmKalin@users.noreply.github.com>
2020-04-16 16:45:03 +0200

    TS & LCD intergation: created functions declarations to markdown active option

--- EKAB/Core/Inc/paintInterface.h
+++ EKAB/Core/Inc/paintInterface.h
@@ -11,8 +11,17 @@
 #include "../../Drivers/BSP/STM32F429I-Discovery/stm32f429i_discovery_lcd.h"
 #include "paint.h"
 
+typedef enum
+{
+	COLOR,
+	THICKNESS
+
+}Markdown_type;
 
 void showPaintInterface();
 void paintService();
+void resetColorMarkdown(void);
+void resetThicknessMarkdown(void);
+void markdown(uint16_t x,uint16_t y,Markdown_type type);
 
 #endif /* INC_PAINTINTERFACE_H_ */
