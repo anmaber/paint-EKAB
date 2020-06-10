9d9b908b123239a19898cf33ec71319a1e0baacf
EmKalin <61980221+EmKalin@users.noreply.github.com>
2020-06-10 12:24:53 +0200

    Created structure that allows to store info which screen is active

---
+++ EKAB/Core/Inc/chooseViewStructure.h
@@ -0,0 +1,21 @@
+/*
+ * chooseViewStructure.h
+ *
+ *  Created on: 10 cze 2020
+ *      Author: ekalinsk
+ */
+
+#ifndef SRC_CHOOSEVIEWSTRUCTURE_H_
+#define SRC_CHOOSEVIEWSTRUCTURE_H_
+
+typedef enum
+{
+	Canvas1,
+	Canvas2,
+	Info,
+	ChooseScreen
+} chooseViewStruct;
+
+chooseViewStruct activeView;
+
+#endif /* SRC_CHOOSEVIEWSTRUCTURE_H_ */
